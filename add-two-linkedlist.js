16006361b4ae5d3bddbf6fe37c25103c58a759be
amyshang <shangxiaohong828@gmail.com>
2022-04-06 12:21:47 -0700

    Update add-two-linkedlist.js

--- add-two-linkedlist.js
+++ add-two-linkedlist.js
@@ -10,6 +10,27 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+const addTwoNumbers2 = (l1, l2) => {
+let node = new ListNode(), copy = node
+    let digits = 0, carry = 0
+    
+    while(l1 || l2) {
+        let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry
+        digits = sum % 10
+        carry = Math.floor(sum / 10)
+        
+        node.next = new ListNode(digits)
+        node = node.next
+        
+        if(l1) l1 = l1.next
+        if(l2) l2 = l2.next
+    }
+    
+    if(carry > 0) node.next = new ListNode(carry)
+    
+return copy.next
+    
+};
 var addTwoNumbers = function(l1, l2) {
     let s1 = '', s2 = '';
     while(l1){
