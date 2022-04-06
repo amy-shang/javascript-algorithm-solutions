0c3a4c1077b99d642454ecc6f434868d11e7ab44
amyshang <shangxiaohong828@gmail.com>
2022-04-06 11:03:14 -0700

    Update string-remove-duplicate.js

--- string-remove-duplicate.js
+++ string-remove-duplicate.js
@@ -1,7 +1,7 @@
-function removeDulicate(str){
-	var length = str.length;
-	var newStr = '';
-	for (var i = 0; i < str.length; i++) {
+const removeDulicate = (str) => {
+	let length = str.length;
+	let newStr = '';
+	for (let i = 0; i < str.length; i++) {
 		if (str.indexOf(str[i]) == str.lastIndexOf(str[i])) {
 			newStr += str[i];
 		};
